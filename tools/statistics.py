97ee9c688ee344ebfe734f3b9b24229946984166
lianghaowei <liang199611@outlook.com>
2017-12-22 17:05:04 +0800

    first commit

---
+++ tools/statistics.py
@@ -0,0 +1,12 @@
+
+def statistics(index_filename,annotations_filename):
+    """
+    draw a statistics pic to see the distribution of class label
+
+
+    :parameter index_filename
+    :parameter annotations_filename a csv file describe the picture name to annotations
+    :return
+
+    """
+    pass
